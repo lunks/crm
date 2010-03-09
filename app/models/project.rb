17b96eac40f3eb90371a4ccb708b47713df8c593
Daniel Lopes <danielvlopes@gmail.com>
2010-03-09 11:39:29 -0300

    adding delegate and select for association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   belongs_to :customer
 
+  delegate :name, :to => :customer, :prefix=>true
+
   validates_associated :customer
   validates_presence_of :customer, :name
   validates_uniqueness_of :name
