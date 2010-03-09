7c7018349e2a140ce93131f5eee54df8ba76d5de
Daniel Lopes <danielvlopes@gmail.com>
2010-03-09 12:03:19 -0300

    finishing aula8

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,9 @@ class Project < ActiveRecord::Base
 
   delegate :name, :to => :customer, :prefix=>true
 
+  named_scope :actives, :conditions => {:active => true}
+  named_scope :inactives, :conditions => {:active => false}
+
   validates_associated :customer
   validates_presence_of :customer, :name
   validates_uniqueness_of :name
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100309142551) do
+ActiveRecord::Schema.define(:version => 20100309145232) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20100309142551) do
     t.integer  "customer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
 end
