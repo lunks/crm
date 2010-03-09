3977065abc01480fb97632b37c49f5fafbf8072c
Daniel Lopes <danielvlopes@gmail.com>
2010-03-09 11:47:32 -0300

    finishing aula7

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.all(:include=>:customer)
 
     respond_to do |format|
       format.html # index.html.erb
