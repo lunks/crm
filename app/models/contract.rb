9d05a2662303a91df26a68693e46a4380a41cf6a
Daniel Lopes <danielvlopes@gmail.com>
2010-03-09 12:10:35 -0300

    model adjustments

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,11 @@
 class Contract < ActiveRecord::Base
-  belongs_to :leader
+  # belongs_to :leader
   belongs_to :project
+
+  validates_presence_of :name, :leader, :project, 
+    :code, :started_at, :description
+    
+  validates_associated :leader, :project
+
+  validates_inclusion_of :status, :in => %w(prospecting accepted canceled)
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ActiveRecord::Base
-  belongs_to :owner
+  # belongs_to :owner
   belongs_to :project
+  
+  validates_presence_of :title, :project_id, :description, :owner_id 
+  validates_numericality_of :progress, :only_integer => true, :allow_nil => true
+    
+  validates_inclusion_of :status, :in => %w(open closed)
+  validates_inclusion_of :priority, :in => %w(normal low high)  
 end
