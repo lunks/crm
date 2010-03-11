2dc4c8c649feb95a99d75d874d7efa4ac9b17c3d
Daniel Lopes <danielvlopes@gmail.com>
2010-03-11 11:42:52 -0300

    finishing ajax part

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,14 @@ class HomeController < ApplicationController
 
   def finish
     sleep 2
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find(params[:id].gsub(/\D/, ""))
     @task.update_attribute("status", "closed")
+  end
+  
+  def save 
+    @task = Task.new(params[:task].merge({ :status => "open" }))
+    @task.owner = current_user
+    @task.save
   end
 
 end
