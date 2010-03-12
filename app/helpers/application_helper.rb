22924a72ee3bd30fc1454f62ca67a1d558955328
Daniel Lopes <danielvlopes@gmail.com>
2010-03-12 02:08:37 -0300

    apply layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
     (0..100).collect{|i| ["#{i}%", i] }
   end
 
+  def flash_notices
+    [:notice, :error, :warning].collect do |type|
+      content_tag('div', flash[type], :class=>"message #{type}", :id => "flash_messages") if flash[type]
+    end
+  end
+
 end
